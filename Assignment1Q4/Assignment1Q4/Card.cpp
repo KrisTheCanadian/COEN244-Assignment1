3c43b42f3ba9741dffee177c98f85d8dd921f8f0
KrisTheCanadian <31254679+KrisTheCanadian@users.noreply.github.com>
2019-09-18 15:49:19 -0400

    Assignment1 Question 4 Completed

--- Assignment1Q4/Assignment1Q4/Card.cpp
+++ Assignment1Q4/Assignment1Q4/Card.cpp
@@ -1,1 +1,46 @@
 #include "Card.h"
+#include <iostream>
+using namespace std;
+void Card::set_suit(char* suit_change) {
+	
+	for (int i = 0; i < SIZE; i++) {
+		suit[i] = suit_change[i];
+	}
+
+}
+
+Card::Card() { //default constructor 
+
+}
+
+char* Card::get_suit() {
+	return suit;
+}
+
+void Card::set_value(int value_change) {
+	value = value_change;
+}
+
+int Card::get_value() {
+	return value;
+}
+
+void Card::display() {
+	
+	if (value < 11 && value > 1) {
+		cout << "[CARD] " << get_value() << "-" << get_suit() << endl;
+	}
+	else {
+		string face;
+		switch (value) {
+			case 1: face = "Ace";
+			case 11: face = "Jack";
+			case 12: face = "Queen";
+			case 13: face = "King";
+			default: face = "ERROR";
+		}
+		
+		cout << "[CARD] " << face << "-" << get_suit() << endl;
+	}
+	
+}
--- Assignment1Q4/Assignment1Q4/Card.h
+++ Assignment1Q4/Assignment1Q4/Card.h
@@ -1,6 +1,16 @@
 #pragma once
 class Card
 {
-
+private:
+	int value; 
+	char suit[8] = {};
+	static const int SIZE = 8;
+public:
+	void set_suit(char*);
+	char* get_suit();
+	void set_value(int);
+	int get_value();
+	void display();
+	Card();
 };
 
